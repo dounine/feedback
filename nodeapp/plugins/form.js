1d57a0814b0966c280697ac00f3bd23aa70b8a22
laohuang <102535481@qq.com>
2016-09-19 16:24:13 +0800

    Update form.js

--- nodeapp/plugins/form.js
+++ nodeapp/plugins/form.js
@@ -22,3 +22,32 @@ module.exports = function (d) {
     }
     return ds;
 }
+//function listForm(prefix,objs) {
+//	if(!objs)return null;
+//	var ds = '';
+//	var f = function(name,o,dx) {
+//		for(var i in o){
+//			var v = o[i];
+//			if((typeof v)==='object'){
+//				f(i,v,dx);
+//			}else{
+//				ds +=(prefix+'['+dx+'].'+name+'.'+i+'='+(v?v:'')+'&');
+//			}
+//		}
+//	}
+//	for(var dx in objs){
+//		for(var i in objs[dx]){
+//			var v = objs[dx][i];
+//			if((typeof v)==='object'){
+//				f(i,v,dx);
+//			}else{
+//				ds += (prefix+'['+dx+'].'+i+'='+v+'&');
+//			}
+//		}
+//	}
+//	return ds;
+//}
+
+//var obj = {name:'nihao',age:23};
+//var objs = [{name:'haha',age:21},{name:'nihao',age:23,info:{data:'123'}}];
+//console.log(listForm('users',objs));
