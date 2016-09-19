2b7dbf474dd06a5c531dc32a74ebb813fc34ade3
laohuang <102535481@qq.com>
2016-09-19 16:43:22 +0800

    Update form.js

--- nodeapp/plugins/form.js
+++ nodeapp/plugins/form.js
@@ -24,30 +24,30 @@ module.exports = function (d) {
 }
 //function listForm(prefix,objs) {
 //	if(!objs)return null;
-//	var ds = '';
+//	var dsArr = [];
 //	var f = function(name,o,dx) {
 //		for(var i in o){
 //			var v = o[i];
-//			if((typeof v)==='object'){
+//			if((typeof v)==='object')
 //				f(i,v,dx);
-//			}else{
-//				ds +=(prefix+'['+dx+'].'+name+'.'+i+'='+(v?v:'')+'&');
-//			}
+//			else
+//				dsArr.push(prefix+'['+dx+'].'+name+'.'+i+'='+(v || ''));
+//			
 //		}
 //	}
 //	for(var dx in objs){
-//		for(var i in objs[dx]){
-//			var v = objs[dx][i];
-//			if((typeof v)==='object'){
+//		var ob = objs[dx];
+//		for(var i in ob){
+//			var v = ob[i];
+//			if((typeof v)==='object')
 //				f(i,v,dx);
-//			}else{
-//				ds += (prefix+'['+dx+'].'+i+'='+v+'&');
-//			}
+//			else
+//				dsArr.push(prefix+'['+dx+'].'+i+'='+v);
+//			
 //		}
 //	}
-//	return ds;
+//	return dsArr.join('&');
 //}
-
-//var obj = {name:'nihao',age:23};
-//var objs = [{name:'haha',age:21},{name:'nihao',age:23,info:{data:'123'}}];
+//
+//var objs = [{name:'haha',age:21},{name:'nihao',age:23,info:{name:1,age:null}}];
 //console.log(listForm('users',objs));
